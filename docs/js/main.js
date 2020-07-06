2603ffcfeab0a57b1ce4b1102673a665560fc450
guy <shaun@shitchell.com>
2020-07-06 17:27:48 -0400

    added transitions

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,61 @@
 var counter = 1;
 var refreshRate = 60000; // 1 minute
 var url = "http://wwc.instacam.com/instacamimg/STNMN/STNMN_l.jpg";
+var oldImg = null;
+var newImg = null;
+var updater = null;
+
+function createImg() {
+	var img = document.createElement("img");
+	img.src = url + "?" + counter + Math.random();
+	img.style.zIndex = counter++;
+	return img;
+}
+
+async function updateImg() {
+	// Replace the current old image with a new invisible image
+	oldImg = newImg;
+	newImg = createImg();
+
+	// Add the new image to the body
+	document.body.appendChild(newImg);
+	// Wait for it to load
+	while (!document.body.contains(newImg))
+	{
+		await new Promise(res => {
+			requestAnimationFrame(resolve)
+		});
+	}
+
+	// Find how long the transition takes
+	const transitionDuration = parseFloat(getComputedStyle(newImg)["transition-duration"]);
+
+	new Promise(res => {
+		newImg.classList.add("visible");
+		setTimeout(() => {
+			res(true);
+		}, transitionDuration * 1000); // Wait for the transition to complete
+	}).then(function() {
+		// Delete the old image
+		if (oldImg !== null)
+		{
+			document.body.removeChild(oldImg);
+		}
+	});
+
+}
+
+function startUpdating() {
+	updater = setInterval(() => {
+		updateImg();
+	}, refreshRate);
+}
+
+function stopUpdating () {
+	clearInterval(updater);
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    var node = document.getElementById("webcam-image");
-
-    setInterval(function() {
-    	node.src = url + "?" + counter++;
-    }, refreshRate);
+	updateImg();
+	startUpdating();
 });
